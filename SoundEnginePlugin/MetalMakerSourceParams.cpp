99fb3f350c25029a692b6ba3a00e59ce76284368
skngh <sknight3637@gmail.com>
2026-09-04 22:56:33 -0700

    param fix

--- SoundEnginePlugin/MetalMakerSourceParams.cpp
+++ SoundEnginePlugin/MetalMakerSourceParams.cpp
@@ -96,19 +96,19 @@ AKRESULT MetalMakerSourceParams::SetParam(AkPluginParamID in_paramID, const void
         m_paramChangeHandler.SetParamChange(PARAM_TYPE_ID);
         break;
     case PARAM_ATTACK_ID:
-        RTPC.fType = *((AkInt32*)in_pValue);
+        RTPC.fType = *((AkReal32*)in_pValue);
         m_paramChangeHandler.SetParamChange(PARAM_ATTACK_ID);
         break;
     case PARAM_DECAY_ID:
-        RTPC.fType = *((AkInt32*)in_pValue);
+        RTPC.fType = *((AkReal32*)in_pValue);
         m_paramChangeHandler.SetParamChange(PARAM_DECAY_ID);
         break;
     case PARAM_SUSTAIN_ID:
-        RTPC.fType = *((AkInt32*)in_pValue);
+        RTPC.fType = *((AkReal32*)in_pValue);
         m_paramChangeHandler.SetParamChange(PARAM_SUSTAIN_ID);
         break;
     case PARAM_RELEASE_ID:
-        RTPC.fType = *((AkInt32*)in_pValue);
+        RTPC.fType = *((AkReal32*)in_pValue);
         m_paramChangeHandler.SetParamChange(PARAM_RELEASE_ID);
         break;
     default:
