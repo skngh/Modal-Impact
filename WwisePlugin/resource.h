3f828944535bcc9585c75bf9d072511df8b51395
Sam Knight <sknight3637@gmail.com>
2026-09-09 15:43:00 -0700

    added all resources

--- WwisePlugin/resource.h
+++ WwisePlugin/resource.h
@@ -5,10 +5,19 @@
 #define IDD_METALMAKERPLUGIN_BIG        5
 #define IDD_METALMAKERPLUGIN_SMALL      105
 #define ID_MODE_GROUP                   1001
-#define IDD_RANDOMNESS                  1002
 #define IDC_RANDOMNESS                  1002
 #define IDC_CHECK_LOOP                  1004
 #define IDC_COMBO_OBJECTTYPE            1005
+#define IDC_GAIN                        1006
+#define IDC_TRANSPOSE                   1007
+#define IDC_LENGTH                      1008
+#define IDC_ATTACK                      1009
+#define IDC_DECAY                       1010
+#define IDC_SUSTAIN                     1011
+#define IDC_RELEASE                     1012
+#define IDC_LPF                         1014
+#define IDC_STATIC24                    1015
+#define IDC_STATIC20                    1015
 
 // Next default values for new objects
 // 
@@ -16,7 +25,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
